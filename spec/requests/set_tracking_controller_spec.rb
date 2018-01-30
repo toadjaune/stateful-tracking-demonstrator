05cef08fd356614697566227d8041af9fd9ec545
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2018-01-30 11:12:08 +0100

    Added a test for redirection of not logged in user

--- spec/requests/set_tracking_controller_spec.rb
+++ spec/requests/set_tracking_controller_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
-  describe "Requests to the set_tracking controller" do
-    login_user
+describe "Requests to the set_tracking controller" do
 
+  describe "when the user is not logged in" do
+    it "redirects the user to the login page" do
+      get set_tracking_path
+      expect(response.status).to eq(302)
+      expect(response).to redirect_to(:new_user_session)
+    end
+  end
+
+  describe "when the user is logged in" do
+    login_user
     describe "on the index action" do
 
       it "returns a 200 for a successful request." do
@@ -11,5 +20,6 @@ require 'rails_helper'
       end
 
     end
-
   end
+
+end
