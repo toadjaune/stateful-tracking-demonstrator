6541e56a0dc5d4ff4b4078e4dcbb9163ae432281
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2018-02-08 20:08:13 +0100

    Tests

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,5 +1,8 @@
 class BaseController < ApplicationController
   def index
+    p request.headers['Host']
+    p request.headers['X-Real-IP']
     p request.headers['X-Forwarded-For']
+    p request.headers['X-https']
   end
 end
