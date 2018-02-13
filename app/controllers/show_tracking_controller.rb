43fe69024374ac134bb9be73c493678e070f7758
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2018-02-13 01:31:53 +0100

    A bit more refactoring

--- app/controllers/show_tracking_controller.rb
+++ app/controllers/show_tracking_controller.rb
@@ -5,10 +5,15 @@ class ShowTrackingController < ApplicationController
 
   def collect_data
     # NB : we prefer creating a new one every time, just in case the session lasts longer than the tracking
-    @tracked_session = TrackedSession.create(session_id: request.session_options[:id])
+    @tracked_session = TrackedSession.new(session_id: request.session_options[:id])
     @hsts_domain_list = Hsts::HSTS_URL_LIST
+
+    check_first_party_cookie
+
+    @tracked_session.save!
   end
 
+  # NB : This method is called by the view, once per hsts subdomain
   def check_hsts
     @tracked_session = TrackedSession.where(id: params[:tracked_session_id]).last
     if request.headers['X-https'] == 'true'
@@ -21,12 +26,12 @@ class ShowTrackingController < ApplicationController
   def display_data
     @tracked_session = TrackedSession.where(session_id: request.session_options[:id]).last
     # Recover all the information about the different tracking methods
-    # We should probably migrate all of this in collect_data
     @methods = {}
 
     # 1st-party cookie
     @methods[:first_party_cookie] = extract_data(FirstPartyCookie.find_by(token: cookies[:tracker]), 'First party Cookie')
 
+    # TODO: migrate this in collect_data
     # LocalStorage
     @methods[:local_storage] = { :name => 'Local Storage' }
     @methods[:local_storage][:worked]  = false
@@ -36,6 +41,10 @@ class ShowTrackingController < ApplicationController
   end
 
   private
+
+  def check_first_party_cookie
+    @tracked_session.first_party_cookie = FirstPartyCookie.find_by(token: cookies[:tracker])
+  end
 
   def extract_data(object, display_name)
     {
