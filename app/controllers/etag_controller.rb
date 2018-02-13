b946f37a3be2594f5e98e302604edcc9fd85c705
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2018-02-13 17:41:49 +0100

    Debugging ETags

--- app/controllers/etag_controller.rb
+++ app/controllers/etag_controller.rb
@@ -8,12 +8,14 @@ class EtagController < ApplicationController
       authenticate_user!
       etag = Etag.create!(user: current_user)
       # TODO : We currently don't worry about about expiration
+      response.set_header('Cache-Control', 'public, max-age=31536000, immutable')
       response.set_header('ETag', etag.token)
     end
 
     if request.headers['Referer']&.include? 'show_tracking'
       tracked_session = TrackedSession.where(session_id: request.session_options[:id]).last
       # NB : We need to make sure every browser uses this header to send back the ETag
+      p request.headers['If-None-Match']
       tracked_session.etag = Etag.find_by(token: request.headers['If-None-Match'])
       tracked_session.save!
       head :not_modified
